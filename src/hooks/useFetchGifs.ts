4ada163364201ad49143aa7c2f04906bfc9346b6
Adrian Gomez Reyes <kunkkaharden@protonmail.com>
2022-11-14 00:04:55 +0100

    useFetchgifs: arreglado tipado del useState

--- src/hooks/useFetchGifs.ts
+++ src/hooks/useFetchGifs.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import getGifs from "../helpers/getGifs";
 import { Gif } from "../components/GifGrid";
 export default function (category: string) {
-  const [state, setState] = useState({
+  const [state, setState] = useState<{data: Gif[], loading: boolean}>({
     data: [],
     loading: true
   });
